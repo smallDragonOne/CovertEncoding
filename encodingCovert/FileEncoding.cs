d3518f8019d3c05017b4065d2a03a9115f3fccd0
zj <1004845067@qq.com>
2019-10-27 21:21:21 +0800

    init

---
+++ encodingCovert/FileEncoding.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encodingCovert
+{
+    public  class FileEncoding
+    {
+
+        public static void StartConvert(string path,bool isFile)
+        {
+            ConvertEncoding(path,isFile);
+            ;
+        }
+
+        public static void ConvertEncoding(string sourceFile, bool isFile, string desFile = "")
+        {
+            var fileList = new List<string>();
+            if (isFile)
+            {
+                fileList.Add(sourceFile);
+            }
+            else
+            {
+
+                fileList = new List<string>(Directory.GetFiles(sourceFile, "*", SearchOption.AllDirectories));
+            }
+
+            var fileQuery = from f in fileList.AsParallel()
+                            let extension = Path.GetExtension(f)
+                            where extension == ".txt" || extension == ".cshtml"
+                            select f;
+            var files = fileQuery.ToList();
+
+            var destFile = string.IsNullOrWhiteSpace(desFile) ? sourceFile : desFile;
+
+            Parallel.ForEach(files, item => {
+                File.WriteAllText(item, File.ReadAllText(item,Encoding.Unicode), Encoding.GetEncoding("UTF-8"));
+            });
+           
+            
+        }
+    }
+}
---
+++ encodingCovert/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace encodingCovert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sourcePath = this.textBox1.Text;
+
+            if (string.IsNullOrWhiteSpace(sourcePath))
+            {
+                MessageBox.Show("路径不能为空");
+                return;
+            }
+
+            var isFile = false;
+            var isPathEnable = false;
+            if (Directory.Exists(sourcePath))
+            {
+                isPathEnable = true;
+            }
+            else if (File.Exists(sourcePath))
+            {
+                isPathEnable = true;
+                isFile = true;
+            }
+
+            if (!isPathEnable)
+            {
+                MessageBox.Show("当前路径不符合要求");
+                return;
+            }
+
+            FileEncoding.StartConvert(sourcePath,isFile);
+            MessageBox.Show("转换成功");
+        }
+    }
+}
